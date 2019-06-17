29fbcb89d63bbcaa54eec3a0610dbdc4df0d1692
teame_11 <teame_11@ele.me>
2019-06-17 23:45:09 +0800

    add

--- src/common.ts
+++ src/common.ts
@@ -7,18 +7,18 @@ export function uuid(len: number, radix?: number) {
 
     if (len) {
         for (i = 0; i < len; i++) {
-            uuid[i] = chars[0 | Math.random() * radix];
+            uuid[ i ] = chars[ 0 | Math.random() * radix ];
         }
     } else {
         let r;
 
-        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-        uuid[14] = '4';
+        uuid[ 8 ] = uuid[ 13 ] = uuid[ 18 ] = uuid[ 23 ] = '-';
+        uuid[ 14 ] = '4';
 
         for (i = 0; i < 36; i++) {
-            if (!uuid[i]) {
+            if (!uuid[ i ]) {
                 r = 0 | Math.random() * 16;
-                uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r];
+                uuid[ i ] = chars[ (i === 19) ? (r & 0x3) | 0x8 : r ];
             }
         }
     }
@@ -27,12 +27,12 @@ export function uuid(len: number, radix?: number) {
 }
 
 // 深度拷贝
-export function deepExtend(out: any, ...args: any) {
+export function deepExtend(out: any, ...args: any[]) {
     if ((typeof out !== 'object' && typeof out !== 'function') || out === null) {
         if (args.length === 0) {
             return out;
         } else if (args.length === 1) {
-            return args[0];
+            return args[ 0 ];
         } else {
             return deepExtend({}, ...args);
         }
@@ -41,7 +41,7 @@ export function deepExtend(out: any, ...args: any) {
     out = out || {};
 
     for (let i = 0; i < args.length; i++) {
-        let obj = args[i];
+        let obj = args[ i ];
 
         if (!obj) {
             continue;
@@ -49,10 +49,10 @@ export function deepExtend(out: any, ...args: any) {
 
         for (let key in obj) {
             if (obj.hasOwnProperty(key)) {
-                if (typeof obj[key] === 'object') {
-                    out[key] = deepExtend(out[key], obj[key]);
+                if (typeof obj[ key ] === 'object') {
+                    out[ key ] = deepExtend(out[ key ], obj[ key ]);
                 } else {
-                    out[key] = obj[key];
+                    out[ key ] = obj[ key ];
                 }
             }
         }
