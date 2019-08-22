118860c952987e5577bad487183ba23105293519
teame_11 <teame_11@ele.me>
2019-08-22 17:04:52 +0800

    add

--- src/decorator/common.ts
+++ src/decorator/common.ts
@@ -1,4 +1,4 @@
-export function Boolean() {
+export function InputBoolean() {
     return function (target: any, propName: string) {
         Object.defineProperty(target, propName, {
             get() {
@@ -12,14 +12,14 @@ export function Boolean() {
     };
 }
 
-export function Number(fallbackValue: number = 0) {
+export function InputNumber(fallbackValue: number = 0) {
     return function (target: any, propName: string) {
         Object.defineProperty(target, propName, {
             get() {
                 return this[ propName ];
             },
             set(value: any) {
-                if (!isNaN(parseFloat(value)) && !isNaN(global.Number(value))) {
+                if (!isNaN(parseFloat(value)) && !isNaN(Number(value))) {
                     this[ propName ] = Number(value);
                 } else {
                     this[ propName ] = fallbackValue;
