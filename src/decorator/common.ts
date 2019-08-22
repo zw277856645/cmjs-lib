3e380aec5a28eb9046fcc016a060df1213c383ac
teame_11 <teame_11@ele.me>
2019-08-22 17:54:42 +0800

    add

--- src/decorator/common.ts
+++ src/decorator/common.ts
@@ -1,30 +1,34 @@
-export function InputBoolean() {
-    return function (target: any, propName: string) {
+function propDecoratorFactory<T, D>(fallback: (v: T) => D): (target: any, propName: string) => void {
+    return function (target: any, propName: string): void {
+        const privatePropName = `__cmjs__decorator__${propName}`;
+
+        Object.defineProperty(target, privatePropName, {
+            configurable: true,
+            writable: true
+        });
+
         Object.defineProperty(target, propName, {
-            get() {
-                return this[ propName ];
+            get(): string {
+                // 使用另一个属性做中转，避免死循环
+                return this[ privatePropName ];
             },
-            set(value: any) {
-                // tslint:disable-next-line:triple-equals
-                this[ propName ] = value != null && `${value}` !== 'false';
+            set(value: T): void {
+                this[ privatePropName ] = fallback(value);
             }
         });
     };
 }
 
+export function InputBoolean() {
+    return propDecoratorFactory((value: any) => value != null && `${value}` !== 'false');
+}
+
 export function InputNumber(fallbackValue: number = 0) {
-    return function (target: any, propName: string) {
-        Object.defineProperty(target, propName, {
-            get() {
-                return this[ propName ];
-            },
-            set(value: any) {
-                if (!isNaN(parseFloat(value)) && !isNaN(Number(value))) {
-                    this[ propName ] = Number(value);
-                } else {
-                    this[ propName ] = fallbackValue;
-                }
-            }
-        });
-    };
+    return propDecoratorFactory((value: any) => {
+        if (!isNaN(parseFloat(value)) && !isNaN(Number(value))) {
+            return Number(value);
+        } else {
+            return fallbackValue;
+        }
+    });
 }
