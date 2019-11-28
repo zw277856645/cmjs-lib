47ff8b792953aac9fafd31ac274d22a81b6ee45a
teame_11 <teame_11@ele.me>
2019-11-28 20:24:36 +0800

    waitFor initial sync

--- src/util/async.ts
+++ src/util/async.ts
@@ -3,17 +3,21 @@ import { first, mapTo, skipWhile } from 'rxjs/operators';
 
 // 等待某条件成立
 export function waitFor<T>(condition: () => boolean, mapValue?: T, limit: number = 15): Observable<T> {
-    return timer(0, 100).pipe(
-        skipWhile(() => {
-            if (limit > 0) {
-                return !condition() && --limit > 0;
-            } else {
-                return !condition();
-            }
-        }),
-        first(),
-        mapTo(mapValue)
-    );
+    if (condition()) {
+        return of(mapValue);
+    } else {
+        return timer(0, 100).pipe(
+            skipWhile(() => {
+                if (limit > 0) {
+                    return !condition() && --limit > 0;
+                } else {
+                    return !condition();
+                }
+            }),
+            first(),
+            mapTo(mapValue)
+        );
+    }
 }
 
 // 各种类型异步转化为 Observable
